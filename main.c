6bcb00cd8e49b408033662d5fba461ab3fb6e140
LucasRohr <lucasrc17@live.com>
2025-09-21 12:11:51 -0300

    feat: :sparkles: add threads creation with server thread

--- main.c
+++ main.c
@@ -10,6 +10,42 @@
 #define DIR_CHECK_INTERVAL 5 // Intervalo em segundos para checar o diretório sync
 #define CURRENT_PEERS 3
 
+// Thread Servidor: Escuta por mensagens
+void* server_thread_func(int* _socket, struct sockaddr_in* my_address) {
+    UDPMessage received_message;
+    struct sockaddr_in sender_address;
+    socklen_t sender_address_len = sizeof(sender_address);
+    int running = 0;
+
+    printf("Thread do servidor iniciada. Escutando por mensagens...\n");
+
+    while (running == 0) {
+        ssize_t bytes_received = recvfrom(*_socket, &received_message, sizeof(received_message), 0,
+                                         (struct sockaddr *)&sender_address, &sender_address_len);
+
+        // Se recebeu exit, encerra thread servidora
+        if (strncmp(received_message.payload, "exit", 4) == 0) {
+            running = 0;
+            break;
+        }
+
+        if (bytes_received > 0) {
+            // Compara para não processar as próprias mensagens de broadcast
+            if (sender_address.sin_addr.s_addr == my_address->sin_addr.s_addr && sender_address.sin_port == my_address->sin_port) {
+                continue;
+            }
+
+            handle_message(*_socket, &received_message, bytes_received, &sender_address); // Chama auxiliar
+        }
+    }
+
+    return NULL;
+}
+
+// Thread Cliente: Verifica o diretório por mudanças ---
+void* client_thread_func(void* arg) {
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Execucao de peer incorreta. Uso correto: %s <meu_ip:porta> <arquivo_peers.conf>\n", argv[0]);
@@ -68,6 +104,20 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Socket UDP criado e vinculado ao endereco/porta: %s.\n", local_ip_port);
+
+    // Inicializa o gerenciador de transferências para segmentos
+    initialize_transfers();
+
+    // Cria as threads
+    pthread_t server_tid, client_tid;
+    pthread_create(&server_tid, NULL, server_thread_func(&_socket, &my_address), NULL);
+    pthread_create(&client_tid, NULL, client_thread_func, NULL);
+
+    // Espera as threads terminarem (quando "exit" for enviado)
+    pthread_join(server_tid, NULL);
+    pthread_join(client_tid, NULL);
+
+    close(_socket);
 
     return 0;
 }
